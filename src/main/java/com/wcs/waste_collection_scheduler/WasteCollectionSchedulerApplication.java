94e01a7c83bd05b1c74cdaa513f22b44549f18d2
ADJASSEM Yao-Fiawomo Justin <justin.adjassem@gmail.com>
2024-09-27 08:19:03 +0000

    files base

---
+++ src/main/java/com/wcs/waste_collection_scheduler/WasteCollectionSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.waste_collection_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteCollectionSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteCollectionSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wcs/waste_collection_scheduler/WasteCollectionSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.waste_collection_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteCollectionSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
